e6cb125d7857717a1bd809335c3aa1c0bed6110f
Rishu Jangra <shrutivishwakarma0509@gmail.com>
2023-01-28 23:46:05 -0500

    styles changed

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const ejs = require("ejs");
 const mongoose=require("mongoose");
 mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const homeStartingContent = "Publish your passions, your way . Create a unique and beautiful blog easily. You can create a blog to inspire, to educate, or to connect with others. But blogging is not just for your readers. It’s also for yourself. There’s a lot of gratification that comes with expressing yourself in new, digital forms.";
+const aboutContent = "This website is made by Shruti Vishwakarma";
+const  contactContent="Contact through LinkedIn or Gmail"
 const app = express();
 const _=require("lodash");
 
@@ -27,26 +26,15 @@ const postSchema={
 const Post=mongoose.model("Post",postSchema);
 
 
-app.get("/",function(req,res){
+app.get("/", function(req, res){
+
   Post.find({}, function(err, posts){
     res.render("home", {
       startingContent: homeStartingContent,
       posts: posts
       });
   });
-  // {aboutCont:aboutContent} ,
-  // {contactCont:contactContent})
-  // console.log(posts)
- 
-
-})
-app.get("/",function(req,res){
-
-
-})
-  // {aboutCont:aboutContent} ,
-  // {contactCont:contactContent})
- 
+});
 
 app.get("/contact",function(req,res){
   res.render("contact",{contactCont:contactContent})
@@ -58,9 +46,7 @@ app.get("/compose",function(req,res){
   res.render("compose")
   // res.redirect("/");
 })
-// app.get("/post",function(req,res){
-//   res.render("post")
-// })
+
 app.get("/posts/:postId", function(req, res){
 
   const requestedPostId = req.params.postId;
